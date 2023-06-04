6da9f7cda58fb4f65737ee75ed0dd4e7691af57c
uabdirasulm <ubbiniyazov04@gmail.com>
2023-06-04 20:03:51 +0500

    optimize code and add comment

--- CheckStringJava.js
+++ CheckStringJava.js
@@ -1,11 +1,15 @@
+/**
+ * Checks if the given string starts with the word "java".
+ *
+ * @param {string} str The string to check.
+ * @returns {boolean} Whether or not the string starts with "java".
+ */
 function isStartWithJava(str) {
+  // Get the first 4 letters of the string and convert them to lowercase.
   const beginningLetters = str.slice(0, 4).toLowerCase();
 
-  if (beginningLetters === "java") {
-    return true;
-  }
-
-  return false;
+  // Return true if the first 4 letters are "java".
+  return beginningLetters === "java";
 }
 
 console.log(isStartWithJava("Javascript"));
